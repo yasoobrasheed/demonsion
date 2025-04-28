f683a59535a73fe0d9f3dc3eafa094e02aaefd20
Yasoob <yasoob@alchemy.com>
2025-04-28 16:15:24 -0500

    Initial Commit

---
+++ demonsion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demonsion
+//
+//  Created by Yasoob Rasheed on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demonsion/demonsionApp.swift
@@ -0,0 +1,17 @@
+//
+//  demonsionApp.swift
+//  demonsion
+//
+//  Created by Yasoob Rasheed on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct demonsionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
